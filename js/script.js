2e4811a78dc7fd9b4785f9c763a609902003ae56
MehrdadKhorasani <mehrdadaria9@gmail.com>
2024-03-27 14:15:53 +0330

    add the fetch functionality

--- js/script.js
+++ js/script.js
@@ -1,10 +1,36 @@
-const URL = "https://jsonplaceholder.typicode.com/posts";
+"strict mode";
+const URL = "js/posts.json";
+const postsEL = document.querySelector(".posts-container");
+
+// fetch(URL)
+//   .then((res) => res.json())
+//   .then((data) => {
+//     data.forEach((post) => {
+//       postsEL.insertAdjacentHTML(
+//         "beforeend",
+//         `
+//         <div class="post">
+//           <h1>${post.title}</h1>
+//           <p>${post.body}</p>
+//         </div>
+//       `
+//       );
+//     });
+//   });
 
 async function getData() {
-  const requset = await fetch(URL);
-  const data = await requset.json();
-  data.forEach((data) => {
-    if (data.id <= 5) console.log(data);
+  const res = await fetch(URL);
+  const data = await res.json();
+  data.forEach((post) => {
+    postsEL.insertAdjacentHTML(
+      "beforeend",
+      `
+        <div class="post">
+          <h1>${post.title}</h1>
+          <p>${post.body}</p>
+        </div>
+      `
+    );
   });
 }
 getData();
